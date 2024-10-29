32196970e96142944f45f0176323e4c1862d0e62
AlondraHackerBae <khrissy921@gmail.com>
2024-10-29 13:56:51 -0500

    added current times&dates to JS with moments.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-//alert("Hello world.");
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+  losAngelesDateElement.innerHTML = losAngelesTime.format("dddd, MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm [<small>]A[</small>]"
+  );
+
+  let miami = document.querySelector("#miami");
+  let miamiDateElement = miami.querySelector(".date");
+  let miamiTimeElement = miami.querySelector(".time");
+  let miamiTime = moment().tz("US/Eastern");
+  miamiDateElement.innerHTML = miamiTime.format("dddd, MMMM Do YYYY");
+  miamiTimeElement.innerHTML = miamiTime.format("h:mm [<small>]A[</small>]");
+
+  let sydney = document.querySelector("#sydney");
+  let sydneyDateElement = sydney.querySelector(".date");
+  let sydneyTimeElement = sydney.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+  sydneyDateElement.innerHTML = sydneyTime.format("dddd, MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format("h:mm [<small>]A[</small>]");
+
+  let rome = document.querySelector("#rome");
+  let romeDate = rome.querySelector(".date");
+  let romeTimeElement = rome.querySelector(".time");
+  let romeTime = moment().tz("Europe/Rome");
+  romeDate.innerHTML = romeTime.format("dddd, MMMM Do YYYY");
+  romeTimeElement.innerHTML = romeTime.format("h:mm [<small>]A[</small>]");
+}
+updateTime();
